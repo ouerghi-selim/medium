b8cf9558053c40f7ce123f4744e6524e72d36209
Selim OUERGHI <ouerghi.selim@gmail.com>
2021-10-09 10:40:04 +0200

    fix bug index

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/")
+ * @Route("/article")
  */
 class ArticleController extends AbstractController
 {
